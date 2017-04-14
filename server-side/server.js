b2a1fcc963d132ccdd329f42eb7d9901cadfe086
Bryan Hernandez <bryan27hr@protonmail.ch>
2017-04-13 22:05:00 -0600

    Nmap connector added basic template for scan, also added modules.json, now we should receive  a list of the modules the user want to enable and work within them

--- server-side/server.js
+++ server-side/server.js
@@ -4,6 +4,7 @@
 var Api = require("./lib/ApiServer");
 var usedmodules = require("./modules.json");
 
+//now we receive this plugin list and we work with it
 var server = new Api({"port":8080, modules: usedmodules.active});
 
 server.start();
