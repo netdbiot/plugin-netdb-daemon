f179ae3ca325d30fe7b3a6c73f5ff4d01d6f96f6
Bryan Hernandez <bryan27hr@protonmail.ch>
2017-04-22 18:54:42 -0600

    Added option to change default port using command line argument

--- server-side/server.js
+++ server-side/server.js
@@ -4,7 +4,13 @@
 var Api = require("./lib/ApiServer");
 var usedmodules = require("./modules.json");
 
+var port = process.argv[2] || 8080;
+if(process.argv[2] == undefined){
+    console.log("No port specified, using 8080");
+    console.log("If you want to use a port (example 6532) run: sudo node server 6532")
+}
+
 //now we receive this plugin list and we work with it
-var server = new Api({"port":8080, modules: usedmodules.active});
+var server = new Api({"port":port, modules: usedmodules.active});
 
 server.start();
